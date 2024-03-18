8ed527a10db4ed52a46cd54c71937c74f56171f2
Sameer Limbu <sameerlimbu31@gmail.com>
2024-03-18 12:25:08 +0000

    added comments

--- server.js
+++ server.js
@@ -36,7 +36,9 @@ app.use('/pic', express.static(path.join(__dirname, 'pic')));
 
 // Static file middleware for lessons images
 app.use("/pic/:imageName", (req, res, next) => {
+  // Extract the image name from the request parameters
   const imageName = req.params.imageName;
+  //Construct image path using dir name and img name
   const imagePath = path.join(__dirname, "pic", imageName);
 
   fs.access(imagePath, fs.constants.F_OK, (err) => {
@@ -88,6 +90,7 @@ connectMongoDB();
 // Middleware to initialize collection based on collectionName parameter
 app.param('collectionName', async (req, res, next, collectionName) => {
   try {
+    //Connect to the db using the collectionName
     req.collection = await connectMongoDB(collectionName);
     return next();
   } catch (err) {
@@ -101,6 +104,7 @@ app.param('collectionName', async (req, res, next, collectionName) => {
 // GET route that returns all the documents in the specified collection
 app.get("/collections/:collectionName", async (req, res) => {
   try {
+    //Get the lessons or orders from the collection stored in the request obj
     const result = await req.collection.find().toArray();
     res.json(result);
   } catch (err) {
@@ -112,8 +116,10 @@ app.get("/collections/:collectionName", async (req, res) => {
 //GET route that returns all the collections with its Id
 app.get("/collections/:collectionName/:id", async (req, res) => {
   try {
+    //Extract id from parameters and parse it to integer 
     const id = parseInt(req.params.id);
 
+    //find a document in the collection with the specified ID
     const result = await req.collection.findOne({ id: id });
     if (result) {
       res.json(result);
@@ -143,56 +149,62 @@ app.post("/submit-order", async (req, res) => {
   }
 });
 
+//PUT for updating the lessons id and spaces when order is submitted
+app.put('/collections/:collectionName', express.json(), async (req, res) => {
+  const ids = req.body.id; // Array of lessonIds
+  const spaces = req.body.spaces; // Array of spaces
+  console.log(ids);
+  try {
+      const promises = ids.map(async (id, index) => {
+          const lessonId = parseInt(id);
+          const space = parseInt(spaces[index]);
+          return req.collection.updateOne({id: lessonId}, {$inc: {availableInventory: -space}});
+      });
+      await Promise.all(promises);
+      res.json({message: 'Lessons updated successfully'});
+  } catch (err) {
+      console.error('Error occurred while updating lessons...\n', err);
+      res.status(500).send('Error occurred while updating lessons');
+  }
+});
+
 //--------------Postman--Testing--------------//
 //POST for creating lessons
 app.post("/collections/:collectionName", async (req, res) => {
   try {
       // Postman testing order data from the request body
-      const { id, title, location, price, image, availableInventory } = req.body;
+      // const { id, title, location, price, image, availableInventory } = req.body;
 
-      // Insert the order into the "orders" collection
+      // Insert the lesson into the "lessonss" collection
       const result = await lessonsCollection.insertOne(req.body);
 
       // Respond with a success message and the order ID
-      res.status(201).json({ message: 'Order submitted successfully', orderId: result.insertedId });
+      res.status(201).json({ message: 'Lesson submitted successfully', orderId: result.insertedId });
   } catch (error) {
       console.error('Error submitting order:', error);
       res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-//PUT for updating the lessons id and spaces
-app.put('/collections/:collectionName', express.json(), async (req, res) => {
-    const ids = req.body.id; // Array of lessonIds
-    const spaces = req.body.spaces; // Array of spaces
-    console.log(ids);
-    try {
-        const promises = ids.map(async (id, index) => {
-            const lessonId = parseInt(id);
-            const space = parseInt(spaces[index]);
-            return req.collection.updateOne({id: lessonId}, {$inc: {availableInventory: -space}});
-        });
-        await Promise.all(promises);
-        res.json({message: 'Lessons updated successfully'});
-    } catch (err) {
-        console.error('Error occurred while updating lessons...\n', err);
-        res.status(500).send('Error occurred while updating lessons');
-    }
-});
 
 //--------------Postman--Testing--------------//
 // PUT for updating the lesson's id, price and availableInventory
 app.put('/collections/:collectionName/:lessonId', express.json(), async (req, res) => {
-  const lessonId = req.params.lessonId;
+  //Extract lesson id from url parameters
+  const lessonId = req.params.lessonId; 
+  
+  //Extract new id, price, space from request body.
   const newId = req.body.id;
   const newPrice = req.body.price;
   const newAvailableInventory = req.body.availableInventory;
 
   try {
     const lessonObjectId = new ObjectId(lessonId);
+    //Update the lesson document and find it by it objectId 
     const result = await req.collection.updateOne(
       { _id: lessonObjectId },
       {
+        //Set new values
         $set: {
           id: newId,
           price: newPrice,
@@ -200,7 +212,7 @@ app.put('/collections/:collectionName/:lessonId', express.json(), async (req, re
         }
       }
     );
-
+    // If the update operation matches exactly one document
     if (result.matchedCount === 1) {
       res.json({ message: 'Lesson updated successfully :)' });
     } else {
@@ -213,12 +225,13 @@ app.put('/collections/:collectionName/:lessonId', express.json(), async (req, re
 });
 
 //--------------Postman--Testing--------------//
-// DELETE for deleting the lesson with its Object_Id
-// DELETE for deleting lessons by _id
+// DELETE for deleting the lessons with its Object_Id
 app.delete('/collections/:collectionName/:lessonId', async (req, res) => {
+  // Extract the lesson ID from the URL parameters
   const lessonId = req.params.lessonId;
 
   try {
+    //Converts into Object ID
     const lessonObjectId = new ObjectId(lessonId);
     const result = await req.collection.deleteMany({ _id: lessonObjectId });
 
